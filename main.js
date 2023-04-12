cf707833bc856158a3a5b1748393be7c7b7b93a2
re3n <94653387+Renboou@users.noreply.github.com>
2023-04-11 19:08:38 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function preload(){
+}
+
+function setup() {
+    canvas = createCanvas(640,480);
+    canvas.position(110,250);
+    tint_color = "";
+}
+
+function draw() {
+tint(tint_color);
+circle(50, 30,800);
+fill("orange")
+circle(50, 30,700);
+fill("yellow")
+circle(50, 30,600);
+fill("green")
+circle(50, 30,500);
+fill("blue")
+circle(50, 30,400);
+fill("purple")
+circle(50, 30,300);
+fill("red")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
